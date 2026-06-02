40f313bff1a0c06f08dfc29d3475b33623580a2b
Alex Owens <alexowens90@gmail.com>
2026-06-02 13:47:07 +0100

    Test with ts index and dynamic schema with different column sets

--- python/tests/unit/arcticdb/version_store/test_compact_data_inline.py
+++ python/tests/unit/arcticdb/version_store/test_compact_data_inline.py
@@ -15,24 +15,58 @@ from arcticdb.util.test import assert_frame_equal
 pytestmark = pytest.mark.pipeline
 
 
-def test_basic(in_memory_store_factory):
-    lib = in_memory_store_factory(dynamic_schema=True)
+@pytest.mark.parametrize("index", [None, "ts"])
+def test_basic(in_memory_store_factory, index):
+    lib = in_memory_store_factory()
     sym = "test_basic"
-    df_0 = pd.DataFrame({"col": np.arange(20)})
+    df_0 = pd.DataFrame(
+        {"col": np.arange(20)}, index=None if index is None else pd.date_range("2026-01-01", periods=20)
+    )
     lib.write(sym, df_0)
-    df_1 = pd.DataFrame({"col": np.arange(20, 30)})
+    df_1 = pd.DataFrame(
+        {"col": np.arange(20, 30)}, index=None if index is None else pd.date_range("2026-01-21", periods=10)
+    )
     lib.append(sym, df_1, compact_data_inline=True)
-    # lib.append(sym, df_1)
-    expected = pd.concat([df_0, df_1]).reset_index(drop=True)
+    expected = pd.concat([df_0, df_1])
+    if index is None:
+        expected = expected.reset_index(drop=True)
+    received = lib.read(sym).data
+    assert_frame_equal(expected, received)
+    assert len(lib.read_index(sym)) == 1
+
+
+@pytest.mark.parametrize("index", [None, "ts"])
+def test_basic_dynamic_schema(in_memory_store_factory, index):
+    lib = in_memory_store_factory(dynamic_schema=True)
+    sym = "test_basic_dynamic_schema"
+    df_0 = pd.DataFrame(
+        {
+            "col_0": np.arange(20, dtype=np.float64),
+            "col_1": np.arange(20, 40, dtype=np.float64),
+            "col_2": np.arange(40, 60, dtype=np.float64),
+        },
+        index=None if index is None else pd.date_range("2026-01-01", periods=20),
+    )
+    lib.write(sym, df_0)
+    df_1 = pd.DataFrame(
+        {"col_2": np.arange(60, 70, dtype=np.float64), "col_1": np.arange(40, 50, dtype=np.float64)},
+        index=None if index is None else pd.date_range("2026-01-21", periods=10),
+    )
+    lib.append(sym, df_1, compact_data_inline=True)
+    expected = pd.concat([df_0, df_1])
+    if index is None:
+        expected = expected.reset_index(drop=True)
     received = lib.read(sym).data
     assert_frame_equal(expected, received)
     assert len(lib.read_index(sym)) == 1
 
 
 # TODO: Tests
+# - with timeseries index
 # - appending an empty df with compact_data_inline=True defrags existing data
 # - appending with row slices NOT attached to the new frame need compacting
 # - with column slicing
 # - with dynamic schema
 # - with data that needs writing (and slicing) after what gets combined with existing data
 # - with fortran-style data
+# - That static/dynamic schema reject appends with mismatching/incompatible schemas
