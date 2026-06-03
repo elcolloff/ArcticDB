1c7d228180aec1979d9e110533b54010ea460f51
Alex Owens <alexowens90@gmail.com>
2026-06-03 14:08:21 +0100

    Parametrize test_defrag_whole_symbol

--- python/tests/unit/arcticdb/version_store/test_compact_data_inline.py
+++ python/tests/unit/arcticdb/version_store/test_compact_data_inline.py
@@ -35,8 +35,7 @@ def test_basic(in_memory_store_factory, index):
     assert len(lib.read_index(sym)) == 1
 
 
-# @pytest.mark.parametrize("index", [None, "ts"])
-@pytest.mark.parametrize("index", [None])
+@pytest.mark.parametrize("index", [None, "ts"])
 def test_defrag_whole_symbol(in_memory_store_factory, index):
     lib = in_memory_store_factory(segment_row_size=10)
     sym = "test_defrag_whole_symbol"
